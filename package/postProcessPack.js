41013952f53ef88eaa2cb3a2ec1c10c9039ad7b2
Zhi_liao <671510496@qq.com>
2025-04-19 22:43:13 +0800

    更换模板+适配
    更换extension.js模板
    postProcessPack.js适配另一边的音频图片整理

--- package/postProcessPack.js
+++ package/postProcessPack.js
@@ -3,8 +3,8 @@ export const postProcessPack = function (pack) {
   const prefixList = ["SK神", "SP神", "SK", "SR", "SP"];
   for (let name in pack.character) {
     if (!pack.character[name][4]) pack.character[name][4] = [];
-    if (!pack.character[name][4].some(j => j.startsWith("die:"))) pack.character[name][4].push(`die:ext:极略/die/${name}.mp3`);
-    pack.character[name][4].push(`${(lib.device || lib.node) ? 'ext:' : 'db:extension-'}极略/${name}.jpg`);
+    if (!pack.character[name][4].some(j => j.startsWith("die:"))) pack.character[name][4].push(`die:ext:极略/audio/die/${name}.mp3`);
+    pack.character[name][4].push(`${(lib.device || lib.node) ? 'ext:' : 'db:extension-'}极略/image/character/${name}.jpg`);
     if (name in pack.translate && !name.startsWith('jlsgsy') && !name.startsWith('jlsgrm')) {
       let translate = pack.translate[name];
       if (!((name + '_ab') in pack.translate)) pack.translate[name + '_ab'] = '极略' + translate;
